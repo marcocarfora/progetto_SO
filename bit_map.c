87edcd96f4f01cc7a58088d5d330c9c0bbabd611
carfora.1794568 <carfora.1794568@studenti.uniroma1.it>
2020-07-24 00:29:58 +0200

    modifica bit_map

--- bit_map.c
+++ bit_map.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include "bit_map.h"
+#include <stdio.h>
 
 // returns the number of bytes to store bits booleans
 int BitMap_getBytes(int bits){
@@ -7,10 +8,11 @@ int BitMap_getBytes(int bits){
 }
 
 // initializes a bitmap on an external array
-void BitMap_init(BitMap* bit_map, int num_bits, uint8_t* buffer){
+void BitMap_init(BitMap* bit_map, int num_bits, int buffer_size, uint8_t* buffer){
   bit_map->buffer=buffer;
   bit_map->num_bits=num_bits;
-  bit_map->buffer_size=BitMap_getBytes(num_bits);
+  bit_map->buffer_size=buffer_size;
+
 }
 
 // sets a the bit bit_num in the bitmap
@@ -19,7 +21,7 @@ void BitMap_setBit(BitMap* bit_map, int bit_num, int status){
   // get byte
   int byte_num=bit_num>>3;
   assert(byte_num<bit_map->buffer_size);
-  int bit_in_byte=byte_num&0x03;
+  int bit_in_byte=7-(bit_num&0x07);  //MODIFICA QUI 07
   if (status) {
     bit_map->buffer[byte_num] |= (1<<bit_in_byte);
   } else {
@@ -31,6 +33,6 @@ void BitMap_setBit(BitMap* bit_map, int bit_num, int status){
 int BitMap_bit(const BitMap* bit_map, int bit_num){
   int byte_num=bit_num>>3; 
   assert(byte_num<bit_map->buffer_size);
-  int bit_in_byte=byte_num&0x03;
+  int bit_in_byte=7-(bit_num&0x07);   //MODIFICA QUI 07  
   return (bit_map->buffer[byte_num] & (1<<bit_in_byte))!=0;
 }
